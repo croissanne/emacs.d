3269357e6233cc2a017ef5350af8e63e03e11a99
sanne raymaekers <sanne.raymaekers@gmail.com>
2017-12-08 12:30:58 +0100

    Some common excludes to ctags-auto-update.

--- modules/init-ctags-update.el
+++ modules/init-ctags-update.el
@@ -3,4 +3,13 @@
 (autoload 'turn-on-ctags-auto-update-mode
   "ctags-update" "turn on 'ctags-auto-update-mode'." t)
 
+(defun configure-ctags-auto-update()
+  (add-to-list 'ctags-update-other-options "--exclude=.build")
+  (add-to-list 'ctags-update-other-options "--exclude=Makefile")
+  (add-to-list 'ctags-update-other-options "--exclude=bin")
+  (add-to-list 'ctags-update-other-options "--exclude=data")
+  (add-to-list 'ctags-update-other-options "--exclude=documentation"))
+
+(add-hook 'ctags-auto-update-mode-hook 'configure-ctags-auto-update)
+
 (provide 'init-ctags-update)
