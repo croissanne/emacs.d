edc5e5ed21391924656bc6bff3bd5369fb5b25c5
sanne raymaekers <sanne.raymaekers@gmail.com>
2017-12-08 13:48:52 +0100

    Confirmation on exit.

--- modules/init-view.el
+++ modules/init-view.el
@@ -1,6 +1,7 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (subword-mode)
+(show-paren-mode)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq inhibit-startup-screen t)
@@ -9,8 +10,7 @@
 (setq column-number-mode t)
 (setq-default show-trailing-whitespace t)
 (setq-default indent-tabs-mode nil)
-(show-paren-mode)
-
+(setq confirm-kill-emacs 'y-or-n-p)
 (add-hook 'term-mode-hook (lambda() (setq-local show-trailing-whitespace nil)))
 
 (provide 'init-view)
