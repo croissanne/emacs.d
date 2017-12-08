d94634c35833b7b0e1e387c4524a6a84bd962697
sanne raymaekers <sanne.raymaekers@gmail.com>
2017-12-08 21:35:01 +0100

    Add keybinding for avy-goto-word.

--- modules/init-avy-mode.el
+++ modules/init-avy-mode.el
@@ -1,6 +1,6 @@
 (ensure-package 'avy)
 
-(define-key custom-keymap (kbd "C-c C-j") 'avy-goto-subword-1)
+(define-key custom-keymap (kbd "C-c C-j") 'avy-goto-word-1)
 (define-key custom-keymap (kbd "C-c j") 'avy-goto-subword-1)
 (define-key custom-keymap (kbd "C-c C-l") 'avy-goto-line)
 (define-key custom-keymap (kbd "C-c l") 'avy-goto-line)
