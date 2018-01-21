15af72e6401a2dbd9566160367f56a6b6e7de3f4
sanne raymaekers <sanne.raymaekers@gmail.com>
2018-01-21 12:11:27 +0100

    Company headers completion.

--- modules/init-company.el
+++ modules/init-company.el
@@ -1,9 +1,24 @@
 (ensure-package 'company)
+(ensure-package 'company-c-headers)
 
 (define-key custom-keymap  (kbd "M-/") 'company-complete)
 
 (global-company-mode)
 (setq company-idle-delay 0.05)
-(setq company-backends (delete 'company-clang company-backends))
+(delete 'company-clang company-backends)
+(add-to-list 'company-backends 'company-c-headers)
+
+;; Example .dir-locals.el for qt5 projects for headers
+;; ((c++-mode
+;;   (eval add-to-list 'company-c-headers-path-system
+;;         "/usr/include/x86_64-linux-gnu/qt5/QtCore")
+;;   (eval add-to-list 'company-c-headers-path-system
+;;         "/usr/include/x86_64-linux-gnu/qt5/QtNetwork")
+;;     (eval add-to-list 'company-c-headers-path-system
+;;         "/usr/include/x86_64-linux-gnu/qt5/QtGui")
+;;   (eval add-to-list 'company-c-headers-path-system
+;;         "/usr/include/x86_64-linux-gnu/qt5/QtWidgets")
+;;   (eval add-to-list 'company-c-headers-path-system
+;;         "/usr/include/x86_64-linux-gnu/qt5/QtTest")))
 
 (provide 'init-company)
