c3a3ed58cae60b7af5f99b7f4bd251e671328c61
sanne raymaekers <sanne.raymaekers@gmail.com>
2018-02-02 10:48:28 +0100

    Auto-fill-mode in markdown.

--- modules/init-markdown-mode.el
+++ modules/init-markdown-mode.el
@@ -2,7 +2,8 @@
 (ensure-package 'markdown-preview-mode)
 
 (defun configure-markdown-mode ()
-  (visual-line-mode))
+  (visual-line-mode)
+  (auto-fill-mode))
 (add-hook 'markdown-mode-hook 'configure-markdown-mode)
 
 (provide 'init-markdown-mode)
