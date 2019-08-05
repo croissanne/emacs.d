584b783c3587921257b5e9f1d8cbfaff941b65df
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2019-08-05 10:05:12 +0200

    Configure scrolling

--- modules/init-view.el
+++ modules/init-view.el
@@ -10,9 +10,12 @@
 (setq-default indent-tabs-mode nil)
 (setq-default confirm-kill-emacs 'y-or-n-p)
 (setq-default fill-column 80)
-(setq-default scroll-step 1)
 (setq-default require-final-newline t)
 (setq-default dired-listing-switches "-lah")
+(setq-default mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq-default mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq-default mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq-default scroll-step 1) ;; keyboard scroll one line at a time
 (add-hook 'term-mode-hook (lambda() (setq-local show-trailing-whitespace nil)))
 
 (provide 'init-view)
