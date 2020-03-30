1bd5e4a01dd6a0207c77fe3691f05cc761ac4fc6
sanne raymaekers <sanne.raymaekers@gmail.com>
2020-03-30 10:14:08 +0200

    Remove js2-mode

--- modules/init-js-mode.el
+++
@@ -1,7 +0,0 @@
-(ensure-package 'js2-mode)
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
-(provide 'init-js-mode)
