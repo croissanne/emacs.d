6ea3b312f678bbeb32da8bcc2c7a59a334485a02
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2021-08-18 13:06:27 +0200

    markdown: Set fill-column to 100

--- modules/init-markdown-mode.el
+++ modules/init-markdown-mode.el
@@ -3,7 +3,8 @@
 
 (defun configure-markdown-mode ()
   (visual-line-mode)
-  (auto-fill-mode))
+  (auto-fill-mode)
+  (setq fill-column 100))
 (add-hook 'markdown-mode-hook 'configure-markdown-mode)
 
 (provide 'init-markdown-mode)
