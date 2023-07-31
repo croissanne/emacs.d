49df5d2d05a158ff333234f1dfd8eac1ef206e62
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-07-31 09:57:44 +0200

    json: add yafolding-mode
    Adding yafolding to the auto-mode list doesn't work for some reason, so
    just add it to the configure hook.

--- modules/init-json-mode.el
+++ modules/init-json-mode.el
@@ -1,10 +1,13 @@
 (ensure-package 'json-mode)
+(ensure-package 'yafolding)
 
 (defun configure-json-mode ()
   (make-local-variable 'js-indent-level)
-  (setq js-indent-level 2))
+  (setq js-indent-level 2)
+  (yafolding-mode))
 
 (add-hook 'json-mode-hook 'configure-json-mode)
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+;; (add-to-list 'auto-mode-alist '("\\.json\\'" . yafolding-mode))
 
 (provide 'init-json-mode)
