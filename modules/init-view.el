b87e570386ff80be3a7bd3ec450f6fa44c2c1a92
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-07-31 09:59:38 +0200

    view: set epg-gpg-program explicitly

--- modules/init-view.el
+++ modules/init-view.el
@@ -18,6 +18,9 @@
 (setq-default mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq-default mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq-default scroll-step 1) ;; keyboard scroll one line at a time
+(setq-default epg-gpg-program "gpg2")
+(setq-default auth-sources
+      '((:source "~/.authinfo.gpg")))
 (add-hook 'term-mode-hook (lambda() (setq-local show-trailing-whitespace nil)))
 
 (provide 'init-view)
