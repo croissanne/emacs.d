b9881d45dae4f47e859036d66e941cf7742ab3e2
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-07-31 16:52:03 +0200

    write: set org-mode font

--- write/modules/init-org-mode.el
+++ write/modules/init-org-mode.el
@@ -1,6 +1,8 @@
 
 (defun configure-org-mode ()
   (visual-line-mode)
+  (setq buffer-face-mode-face '(:family "Iosveka Etaoile" :height 140 :width))
+  (buffer-face-mode)
   (writeroom-mode))
 (add-hook 'org-mode-hook 'configure-org-mode)
 
