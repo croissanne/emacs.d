184fdbd56fcfb2b76f42b0e88056f6056deec9d2
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-12-14 16:57:44 +0100

    work/view: fill-column, split threshold, so-long mode

--- work/modules/init-view.el
+++ work/modules/init-view.el
@@ -12,9 +12,10 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-bar-mode nil)
 (setq-default confirm-kill-emacs 'y-or-n-p)
-(setq-default fill-column 120)
+(setq-default fill-column 100)
 (setq-default require-final-newline t)
 (setq-default dired-listing-switches "-lah")
+(setq-default split-width-threshold 120)
 (setq-default ring-bell-function 'ignore) ;; disable audio notifications
 (setq-default mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 (setq-default mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
@@ -23,9 +24,16 @@
 (setq-default epg-gpg-program "gpg2")
 (setq-default auth-sources
       '((:source "~/.authinfo.gpg")))
+(setq-default scroll-step 1 scroll-conservatively  10000)
 (add-hook 'term-mode-hook (lambda() (setq-local show-trailing-whitespace nil)))
+
+(setq-default frame-title-format '("%f"))
+
+;; Avoid performance issues in files with very long lines.
+(global-so-long-mode 1)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/common/themes")
 (load-theme 'sanne t)
 
 (provide 'init-view)
+
